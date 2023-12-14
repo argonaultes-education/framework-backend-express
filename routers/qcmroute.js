4551e162a01f6d7d170fbe044ef74ceea6028dc8
Kernel-rb <matabsaifeddine@gmail.com>
2023-12-14 11:04:32 +0100

    first commit

--- routers/qcmroute.js
+++ routers/qcmroute.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-
-const {displayQcms, displayFormQcm, createNewForm, displayQcmJson, displayQcmDetailed} = require('../controllers/qcms');
-
-
-// DEFINITION DES ROUTES
+const {
+    displayQcms,
+    displayFormQcm,
+    createNewForm,
+    displayQcmJson,
+    displayQcmDetailed,
+} = require('../controllers/qcms');
 
 router.get('/', displayQcms);
-
-//point d'acces pour afficher le detail d'un QCM
-router.get('/:qcmid', displayQcmDetailed)
-// permet l'utilisation de localhost:3000/qcms/2
-// qui affiche le qcm ayant pour id 2
-// req.params.qcmid
-router.get('/json', displayQcmJson);
-
-router.get('/new', displayFormQcm); //handler
-
+router.get('/new', displayFormQcm); // This line is added
 router.post('/new', createNewForm);
-
-
-//FIN DES ROUTES
-
+router.get('/:qcmid', displayQcmDetailed);
+router.get('/json', displayQcmJson);
 
 module.exports = router;
--- routers/welcomeroute.js
+++ routers/welcomeroute.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {welcomeMessage, welcomeMessageFunction, testEjsView} = require('../controllers/welcome');
-
-// DEBUT DES ROUTES
+const {
+  welcomeMessage,
+  welcomeMessageFunction,
+  testEjsView,
+} = require('../controllers/welcome');
 
 router.get('/', welcomeMessage);
-
 router.get('/welcome2', welcomeMessageFunction);
-
 router.get('/testejs', testEjsView);
-
-//FIN DES ROUTES
 
 module.exports = router;
